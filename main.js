1967a3ecd0f5c2418d564c26deb2397821cd9b9b
Matthew Falbo <matthew.falbo@gmail.com>
2014-04-24 14:14:21 -0600

    Added basic structure. CSS Transform Functionality. Construct quote list from textarea inputs.

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 $(document).on('ready', function() {
+
+	$('#quote-form').on('submit', function(){
+		var quote = $(this).find('#quote-area').val();
+		// console.log($(this).find('#quote-area').val());
+		var quoteItem = $('<li></li>');
+		var quoteContent = $('<blockquote></blockquote>');
+		quoteContent.text(quote);
+		quoteItem.append(quoteContent)
+		$(this).closest('.section').find('.quote-list').append(quoteItem);
+
+		$(this).find('#quote-area').val('');
+		$(this).find('#quote-area').focus();
+
+		return false;
+	})
   
 });
+
+
+
+
+
+
+// 
+// 
